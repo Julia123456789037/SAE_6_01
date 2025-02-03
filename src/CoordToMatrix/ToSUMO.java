cb988fb14d63789454457e0974d224e10534ec34
Ashanti Njanja <shantil_ytb@outlook.com>
2025-02-03 17:08:08 +0100

    generation fichier route xml

--- src/CoordToMatrix/ToSUMO.java
+++ src/CoordToMatrix/ToSUMO.java
@@ -1,19 +1,22 @@
 package CoordToMatrix;
 
-public class ToNetXML
+public class ToSUMO
 {
-    public static final String EXTENSION = ".net.xml";
+    public static final String EXTENSION_GRAPH = ".net.xml";
+    public static final String EXTENSION_ROUTE = ".rou.xml";
 
     private CoordToMatrix ctm;
+    // private Resultat resultat;
 
-    public ToNetXML(CoordToMatrix ctm)
+    public ToSUMO(CoordToMatrix ctm)
     {
         this.ctm = ctm;
+        // TODO :this.resultat = ...;
     }
 
-    public String getFormat()
+    public String getNetXML()
     {
-		String text =
+		String retStr =
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n\r\n"                                                                                                               +
         "<!-- generated on "+ CoordToMatrix.getDate() +" by MAJ for SUMO netedit Version 1.15.0\r\n"                                                                       +
         "<configuration xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://sumo.dlr.de/xsd/netconvertConfiguration.xsd\">\r\n" +
@@ -48,7 +51,7 @@ public class ToNetXML
         "</configuration>\r\n"                                                                           +
         "-->\r\n\r\n";
         
-        text += "<net version=\"1.9\" junctionCornerDetail=\"5\" limitTurnSpeed=\"5.50\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://sumo.dlr.de/xsd/net_file.xsd\">\r\n\r\n"; 
+        retStr += "<net version=\"1.9\" junctionCornerDetail=\"5\" limitTurnSpeed=\"5.50\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://sumo.dlr.de/xsd/net_file.xsd\">\r\n\r\n"; 
         
         // Itération sur les routes entre les clients
         for (int i = 0; i < this.ctm.matrix.length; i++) {
@@ -56,23 +59,50 @@ public class ToNetXML
                 if (j == i) continue;
 
                 int index = i * this.ctm.matrix.length + j;
-                text += "\t<edge id=\"E" + index + "\" from=\"" + i + "\" to=\"" + j + "\" priority=\"-1\">\r\n" +
+                retStr += "\t<edge id=\"E" + index + "\" from=\"" + i + "\" to=\"" + j + "\" priority=\"-1\">\r\n" +
                         "\t\t<lane id=\"E" + index + "_\" index=\"0\" speed=\"13.89\" width=\"0.50\" length=\""+ this.ctm.points[i].getDistance(this.ctm.points[j]) *10 +"\" shape=\"-32.83,65.57 32.11,60.58\"/>\r\n" +
                         "\t</edge>\r\n\r\n";
             }
         }
 
-        text += "\r\n";
+        retStr += "\r\n";
 
         // Itération sur tous les clients
         for (int i = 0; i < this.ctm.points.length; i++) {
             Point  p = this.ctm.points[i];
             String c = (i < 26) ? (char) ('A' + i -1) + "" : ((char) ('A' + ((i -1) / 26) -1)) + "" + ((char) ('A' + ((i -1) % 26)));
 
-            text += "\t<junction id=\"" + i +"\" type=\"priority\" x=\""+ p.x() *10 +"\" y=\""+ p.y() *10 +"\" incLanes=\"\" intLanes=\"\" shape=\"142.73,46.58\"/>\r\n";
+            retStr += "\t<junction id=\"" + i +"\" type=\"priority\" x=\""+ p.x() *10 +"\" y=\""+ p.y() *10 +"\" incLanes=\"\" intLanes=\"\" shape=\"142.73,46.58\"/>\r\n";
         }
 
-        text += "\r\n</net>"; 
-        return text;
+        retStr += "\r\n</net>"; 
+        return retStr;
+    }
+
+    public String getRouXML()
+    {
+		String retStr =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n\r\n"                                         +
+        "<!-- generated on "+ CoordToMatrix.getDate() +" by MAJ for SUMO netedit Version 1.15.0\r\n" +
+        "-->\r\n\r\n";
+        
+        retStr += "<routes xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://sumo.dlr.de/xsd/routes_file.xsd\">\r\n";
+        retStr += "\t<!-- Vehicles, persons and containers (sorted by depart) -->\r\n"; 
+        
+        // Itération sur les routes prisent par les véhicules
+        // TODO : Une route est composé de plusieurs arrêtes
+
+        /*
+        for (int i = 0; i < this.resultat.getRoutes().length; i++) {
+            Route route = this.resultat.getRoute(i);
+            retStr += 
+            "\t<trip id=\"t_"+ i +"\" depart=\"0.00\" from=\"" + route.getLstArrete().get(0) + "\" to=\"" + route.getLstArrete().get(route.getLstArrete().size() -1) +
+            "\" via=\"" + String.join(" ", route.getLstArrete()) + "\"/>";
+
+            retStr += "\r\n";
+        } */
+
+        retStr += "</routes>"; 
+        return retStr;
     }
 }
